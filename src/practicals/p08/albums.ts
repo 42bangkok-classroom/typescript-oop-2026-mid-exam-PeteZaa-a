2069c72a899d6abe6a6213d7b38b2d1fbeee277a
Pasakorn Cheeraachanon <68010862@kmitl.ac.th>
2026-01-21 16:55:35 +0700

    commit

--- src/practicals/p08/albums.ts
+++ src/practicals/p08/albums.ts
@@ -1,6 +1,30 @@
-interface FullAlbum {}
+import axios from "axios";
+
+interface FullAlbum {
+    userId:number
+    id:number
+    title:number
+    photos: Object
+}
 
 const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos';
 const ALBUMS_URL = 'https://jsonplaceholder.typicode.com/albums';
 
-function mapPhotoToAlbum(userIds?: number[]): Promise<FullAlbum[]>
+export async function mapPhotoToAlbum(userIds?: number[]): Promise<FullAlbum[]> {
+    try {
+        const getAlbum = await axios.get(ALBUMS_URL)
+
+        const getPhotos = await axios.get(PHOTOS_URL)
+
+        const albumData = getAlbum.data.userId
+
+        
+
+    } catch (error) {
+        throw new Error ("Errror rrr")
+    }
+}
+
+
+
+mapPhotoToAlbum()
