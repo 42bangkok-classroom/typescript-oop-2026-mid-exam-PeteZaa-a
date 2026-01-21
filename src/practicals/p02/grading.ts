ecc2543ac077e6111f3e39bf0b0220ccada95778
Pasakorn Cheeraachanon <68010862@kmitl.ac.th>
2026-01-21 15:28:27 +0700

    commit

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -9,22 +9,22 @@ if (isNaN(input)) {
 else {
 
 if (input >= 80 && input <= 100) {
-    console.log("A");
+    console.log("Grade is A");
 }
 else if (input >=70 && input < 80) {
-    console.log("B");
+    console.log("Grade is B");
     
 }
 else if (input >=60 && input < 70) {
-    console.log("C");
+    console.log("Grade is C");
     
 }
 else if (input >=50 && input < 60) {
-    console.log("D");
+    console.log("Grade is D");
     
 }
 else if (input > 0 && input < 50) {
-    console.log("F");
+    console.log("Grade is F");
     
 }
 else {
